f1acd41c413c5d980bf1c9d64c35ac364b16cbd1
jaylee <syuueih@gmail.com>
2022-02-21 10:58:47 +0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+console.log(privateKey);
+console.log(infuraId);
+// const fs = require('fs');
+// const privateKey = ""; //private key of ropsten network address
+
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat:{
+      chainId:1337
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${infuraId}`, //https://ropsten.infura.io/v3/... infura project link <= ENDPOINTS = ROPSTEN
+      accounts: [`0x${privateKey}`]
+    }
+  }
+};
+
